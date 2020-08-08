31a018ce5b48f5334510bbef914718ccbf294b19
CoderKazama <majidleex9800@gmail.com>
2020-08-08 08:51:55 +0530

    Updates

---
+++ precache-manifest.bb65a9456aaa6322e51ab4c13564c6d9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b2b62092e3a0af0019ce741dac0512f",
+    "url": "/captionking/index.html"
+  },
+  {
+    "revision": "cc6156c3b05b2f0e843e",
+    "url": "/captionking/static/css/main.bd8c3ade.chunk.css"
+  },
+  {
+    "revision": "fa1e2b67469c4b6af7dc",
+    "url": "/captionking/static/js/2.ca58059c.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/captionking/static/js/2.ca58059c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc6156c3b05b2f0e843e",
+    "url": "/captionking/static/js/main.4f05f9a9.chunk.js"
+  },
+  {
+    "revision": "f96cee695a8262a74cca",
+    "url": "/captionking/static/js/runtime-main.1e5d9d80.js"
+  },
+  {
+    "revision": "d3c37b66b39dd48ec2c2b65f87a97c4d",
+    "url": "/captionking/static/media/app.d3c37b66.png"
+  },
+  {
+    "revision": "6890f0c5813b359db24a2cff1c596a3b",
+    "url": "/captionking/static/media/header.6890f0c5.jpg"
+  },
+  {
+    "revision": "a3bb9e68c8a6b2d2423a0727d4839fa1",
+    "url": "/captionking/static/media/logo.a3bb9e68.png"
+  }
+]);
